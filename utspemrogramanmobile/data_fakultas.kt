7add628dfb8e95bca6e5a5de507b4ea2411da367
MRizalAbdullahRozi <61196689+MRizalAbdullahRozi@users.noreply.github.com>
2020-11-03 17:03:20 +0700

    Add files via upload

---
+++ utspemrogramanmobile/data_fakultas.kt
@@ -0,0 +1,10 @@
+package com.example.utspemrogramanmobile
+
+class data_fakultas
+
+    (
+    val imgFak: Int,
+    val namaFakultas: String,
+    val DescFakultas: String,
+    val descprodi: String
+)
